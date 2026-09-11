5f5d610f5759e06355d656974587e5c1f1cb0ec3
ranjaysingh2121-spec <ranjaysingh2121@gmail.com>
2026-09-11 15:16:25 +0530

    Add files via upload

---
+++ week2homeworkproblem/P5.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class P5 {
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        feedback = feedback.toLowerCase();
+
+        feedback = feedback.replace(".", "");
+        feedback = feedback.replace(",", "");
+
+        String[] words = feedback.split("\\s+");
+
+        String[] stopWords = {
+            "the", "was", "and", "a", "is", "of", "in"
+        };
+
+        HashMap<String, Integer> frequency = new HashMap<>();
+
+        for (String word : words) {
+
+            boolean isStopWord = false;
+
+            for (String stopWord : stopWords) {
+                if (word.equals(stopWord)) {
+                    isStopWord = true;
+                    break;
+                }
+            }
+
+            if (!isStopWord) {
+                if (frequency.containsKey(word)) {
+                    frequency.put(word, frequency.get(word) + 1);
+                } else {
+                    frequency.put(word, 1);
+                }
+            }
+        }
+
+        ArrayList<Map.Entry<String, Integer>> list =
+                new ArrayList<>(frequency.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String feedback =
+                "The mentor was great, the session was great and clear.";
+
+        printFilteredWordFrequency(feedback);
+    }
+}
