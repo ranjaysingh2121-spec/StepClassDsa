6634c850438c7eb2ae61fde0f838321363608418
ranjaysingh2121-spec <ranjaysingh2121@gmail.com>
2026-09-12 16:24:30 +0530

    Add week 4 homework problems

---
+++ week4homeworkproblem/P1.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class P1 {
+
+    static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        // Product of elements on the left
+        answer[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        // Product of elements on the right
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
---
+++ week4homeworkproblem/P4.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+public class P4 {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Empty prefix sum
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            currentSum = currentSum + nums[i];
+
+            // Check whether currentSum - k exists
+            if (map.containsKey(currentSum - k)) {
+                count = count + map.get(currentSum - k);
+            }
+
+            // Store frequency of current prefix sum
+            if (map.containsKey(currentSum)) {
+                map.put(currentSum, map.get(currentSum) + 1);
+            } else {
+                map.put(currentSum, 1);
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        System.out.println(subarraySum(nums, k));
+    }
+}
